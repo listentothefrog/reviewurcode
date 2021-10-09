d75889ccd35e481c993ff7979034c7a9cac5b19f
listentothefrog <shashanke7y@gmail.com>
2021-10-09 16:56:33 -0400

    login page

---
+++ components/landing-page/LoginPage.tsx
@@ -0,0 +1,44 @@
+import GithubIcon from "../../icons/Github";
+
+const LoginPage = () => {
+  return (
+    <div className="relative bg-white overflow-hidden">
+      <div className="flex max-w-7xl mx-auto">
+        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+            <div className="sm:text-center lg:text-left">
+              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                <span className="block xl:inline">An online platform to </span>{" "}
+                <span className="block text-red-500 xl:inline">
+                  review your code
+                </span>
+              </h1>
+              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                <span className="font-bold">ReviewurCode</span> is a online
+                platfrom where programmers can review code and submit feedback
+                to make your code more futuristic.
+              </p>
+              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                <div className="rounded-md shadow">
+                  <button className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700 md:py-4 md:text-lg md:px-10">
+                    <GithubIcon width="24" height="20" />
+                    <span className="ml-3">Login with Github</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <img
+          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+          src="https://images.unsplash.com/photo-1602992708529-c9fdb12905c9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80"
+          alt="coder"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
