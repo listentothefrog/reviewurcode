f29ec858efdbf85e97110af7f5916786104deea6
listentothefrog <shashanke7y@gmail.com>
2021-10-10 11:33:57 -0400

    changed image to a group coding

--- components/landing-page/LoginPage.tsx
+++ components/landing-page/LoginPage.tsx
@@ -37,8 +37,8 @@ const LoginPage = () => {
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <img
           className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src="https://images.unsplash.com/photo-1602992708529-c9fdb12905c9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80"
-          alt="coder"
+          src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
+          alt="codeing group"
         />
       </div>
     </div>
