becc1f6f953c0f2114040b9cd65132a8fa5fd82a
listentothefrog <shashanke7y@gmail.com>
2021-10-13 14:54:57 -0400

    dashboard page route

---
+++ pages/dash.tsx
@@ -0,0 +1,16 @@
+import { useRouter } from "next/dist/client/router";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../lib/firebase/firebase";
+
+const dash = () => {
+  const [user, loading, error] = useAuthState(auth);
+  const router = useRouter();
+  if (loading) return <h1 className="text-red-500">Loading...</h1>;
+  else if (error) return console.log(error);
+  else if (!user) {
+    router.push("/");
+  }
+  return <div>hello world</div>;
+};
+
+export default dash;
