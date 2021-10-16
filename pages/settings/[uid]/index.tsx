7d5473d19375621bbd4b25b7847275009a3eddd1
listentothefrog <shashanke7y@gmail.com>
2021-10-15 21:02:33 -0400

    metadata for setting page

--- pages/settings/[uid]/index.tsx
+++ pages/settings/[uid]/index.tsx
@@ -2,6 +2,8 @@ import { useRouter } from "next/dist/client/router";
 import { useAuthState } from "react-firebase-hooks/auth";
 import UserSettings from "../../../components/settings/UserSettings";
 import { auth } from "../../../lib/firebase/firebase";
+import Head from "next/head";
+import Logo from "../../../public/RedStone.png";
 
 const index = () => {
   const [user, loading, error] = useAuthState(auth);
@@ -11,7 +13,16 @@ const index = () => {
   else if (!user) {
     router.push("/");
   }
-  return <UserSettings username={auth.currentUser?.displayName} />;
+  return (
+    <>
+      <Head>
+        <title>Settings for {auth.currentUser?.displayName}</title>
+        <link rel="shortcut icon" href={Logo.src} type="image/x-icon" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <UserSettings username={auth.currentUser?.displayName} />
+    </>
+  );
 };
 
 export default index;
