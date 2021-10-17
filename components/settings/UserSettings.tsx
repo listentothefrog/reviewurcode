e61a855f41e66b23f70839f17f37443d8a185607
listentothefrog <shashanke7y@gmail.com>
2021-10-16 22:46:43 -0400

    make signout function aynsc

--- components/settings/UserSettings.tsx
+++ components/settings/UserSettings.tsx
@@ -26,7 +26,7 @@ const UserSettings = () => {
         <div>
           <button
             className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
-            onClick={() => signOut(auth)}
+            onClick={async () => await signOut(auth)}
           >
             Sign out
           </button>
