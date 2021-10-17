2de97e859b13289e5e70ce92582816ba12a8bce1
listentothefrog <shashanke7y@gmail.com>
2021-10-17 12:51:10 -0400

    added metadata and open graph tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,56 @@
 import LoginPage from "../components/landing-page/LoginPage";
-
+import Head from "next/head";
+import RedStone from "../public/RedStone.png";
 const Home = () => {
-  return <LoginPage />;
+  return (
+    <>
+      <Head>
+        <title>ReviewurCode — Online platform for code review</title>
+        <link rel="shortcut icon" href={RedStone.src} type="image/x-icon" />
+        <meta
+          name="title"
+          content="ReviewurCode — Online platform for code review"
+        />
+        <meta
+          name="description"
+          content="ReviewurCode is an online platform where programmers can ask for a code review or review other's code."
+        />
+
+        {/* Open Graph / Facebook  */}
+        <meta property="og:type" content="website" />
+        {/* <meta property="og:url" content="" /> */}
+        <meta
+          property="og:title"
+          content="ReviewurCode — Online platform for code review"
+        />
+        <meta
+          property="og:description"
+          content="ReviewurCode is an online platform where programmers can ask for a code review or review other's code."
+        />
+        <meta
+          property="og:image"
+          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+        />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        {/* <meta property="twitter:url" content="https://metatags.io/" /> */}
+        <meta
+          property="twitter:title"
+          content="ReviewurCode — Online platform for code review"
+        />
+        <meta
+          property="twitter:description"
+          content="ReviewurCode is an online platform where programmers can ask for a code review or review other's code. "
+        />
+        <meta
+          property="twitter:image"
+          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+        />
+      </Head>
+      <LoginPage />
+    </>
+  );
 };
 
 export default Home;
