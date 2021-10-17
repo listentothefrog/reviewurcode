762b686812286310855d730880d3d9e5f1750ae6
listentothefrog <shashanke7y@gmail.com>
2021-10-17 14:55:04 -0400

    remove metadata image url

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,10 +30,7 @@ const Home = () => {
           property="og:description"
           content="ReviewurCode is a online platfrom where programmers can review code and submit feedback to make your code more futuristic."
         />
-        <meta
-          property="og:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        />
+        <meta property="og:image" content="https://ibb.co/J3954sn" />
 
         {/* Twitter */}
         <meta property="twitter:card" content="summary_large_image" />
@@ -46,10 +43,7 @@ const Home = () => {
           property="twitter:description"
           content="ReviewurCode is a online platfrom where programmers can review code and submit feedback to make your code more futuristic."
         />
-        <meta
-          property="twitter:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        />
+        <meta property="twitter:image" content="https://ibb.co/J3954sn" />
       </Head>
       ;
       <LoginPage />
