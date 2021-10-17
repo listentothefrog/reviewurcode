16c284c96dd81995a88565ed9cd465118c9b37b9
listentothefrog <shashanke7y@gmail.com>
2021-10-16 21:10:28 -0400

    contact form component

---
+++ components/contact/ViewContactForm.tsx
@@ -0,0 +1,37 @@
+import Container from "../Container";
+import {
+  EmailInputFeild,
+  MessageInputFeild,
+  YourNameInputFeild,
+} from "./FormField";
+
+const ViewContactForm = () => {
+  return (
+    <Container variant="regular">
+      <div className="flex flex-col items-center justify-center p-2">
+        <div className="flex flex-col items-start justify-center">
+          <h1 className="text-5xl sm:text-4xl font-bold">
+            Get in touch with us
+          </h1>
+          <p className="mt-3 text-base sm:text-lg text-gray-500">
+            We'd love to hear form you! Send us a message using the form
+            oppisite, or email us. Ask us anything about the product and what we
+            do, your thoughts are not limited!
+          </p>
+        </div>
+        <form className="flex items-start flex-1 flex-col w-full mt-3">
+          <YourNameInputFeild />
+          <EmailInputFeild />
+          <MessageInputFeild />
+          <div className="w-full">
+            <button className="w-full py-3 rounded-md text-white bg-red-500 hover:bg-red-400 transition duration-500 ease-in-out  mt-3">
+              Swooosh 💨
+            </button>
+          </div>
+        </form>
+      </div>
+    </Container>
+  );
+};
+
+export default ViewContactForm;
