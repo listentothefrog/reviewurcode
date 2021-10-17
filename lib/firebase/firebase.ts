fe3112fc04cf6383634a5c746d8b8f4e99e27dc8
listentothefrog <shashanke7y@gmail.com>
2021-10-16 22:04:38 -0400

    export firestore variable

--- lib/firebase/firebase.ts
+++ lib/firebase/firebase.ts
@@ -1,9 +1,10 @@
 import firebaseConfig from "./firebaseConfig";
-
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 if (!getApps.length) {
   initializeApp(firebaseConfig);
 }
 export const auth = getAuth();
+export const db = getFirestore();
