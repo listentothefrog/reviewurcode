b7ed24f16d0dc31529644bb4df2c5cef7c59e522
listentothefrog <shashanke7y@gmail.com>
2021-10-18 19:52:29 -0400

    added metadata

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,15 +1,27 @@
-import React, { Suspense } from "react";
-
-const ViewContactForm = React.lazy(
+import dynamic from "next/dynamic";
+const ViewContactForm = dynamic(
   () => import("../components/contact/ViewContactForm")
 );
+import Head from "next/head";
+import RedStone from "../public/RedStone.png";
 
 const contact = () => {
   return (
     <>
-      <Suspense fallback="Loading...">
-        <ViewContactForm />
-      </Suspense>
+      <Head>
+        <title>ReviewurCode — Online platform for code review</title>
+        <link rel="shortcut icon" href={RedStone.src} type="image/x-icon" />
+        <html lang="en" />
+        <meta
+          name="title"
+          content="ReviewurCode — Online platform for code review"
+        />
+        <meta
+          name="description"
+          content="ReviewurCode is a online platfrom where programmers can review code and submit feedback to make your code more futuristic."
+        />
+      </Head>
+      <ViewContactForm />
     </>
   );
 };
