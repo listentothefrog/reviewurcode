cbbf7769a7df1849caf2a55fc57c927edde85a08
listentothefrog <shashanke7y@gmail.com>
2021-10-23 17:20:25 -0400

    change heading text

--- components/Dashboard/ViewFeed.tsx
+++ components/Dashboard/ViewFeed.tsx
@@ -7,7 +7,7 @@ const ViewFeed = () => {
     <Container variant="regular">
       <div className="flex flex-col items-center justify-between m-2">
         <div className="flex items-center justify-between w-full h-full">
-          <h1 className="text-xl font-medium">Top Questions</h1>
+          <h1 className="text-xl font-medium">Top Posts</h1>
           <button className="px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-500 hover:bg-red-600 md:py-4 md:text-lg md:px-10 transition duration-500 ease-in-out">
             Create a post
           </button>
