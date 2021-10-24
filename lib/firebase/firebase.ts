5d633b38aa0a88878e538a32b6a3580c73526143
listentothefrog <shashanke7y@gmail.com>
2021-10-24 13:47:49 -0400

    removed firebase config file

--- lib/firebase/firebase.ts
+++ lib/firebase/firebase.ts
@@ -1,10 +1,16 @@
-import firebaseConfig from "./firebaseConfig";
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-if (!getApps.length) {
-  initializeApp(firebaseConfig);
-}
-export const auth = getAuth();
-export const db = getFirestore();
+const firebaseConfig = initializeApp({
+  apiKey: "AIzaSyDDT5wWsfi6REi0ZQkxOQcRJVWGpo3dvD0",
+  authDomain: "reviewurcode.firebaseapp.com",
+  projectId: "reviewurcode",
+  storageBucket: "reviewurcode.appspot.com",
+  messagingSenderId: "844904836127",
+  appId: "1:844904836127:web:51575813301c1b01e1b92a",
+  measurementId: "G-4S48QKTQGN",
+});
+
+export const auth = getAuth(firebaseConfig);
+export const db: any = getFirestore(firebaseConfig);
