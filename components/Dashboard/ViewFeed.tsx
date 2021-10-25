99a56e91f9144e299489b4f81692bca686b42672
listentothefrog <shashanke7y@gmail.com>
2021-10-25 13:41:25 -0400

    fetch data from firestore database

--- components/Dashboard/ViewFeed.tsx
+++ components/Dashboard/ViewFeed.tsx
@@ -1,8 +1,27 @@
 import dynamic from "next/dynamic";
 import Container from "../Container";
+import { db } from "../../lib/firebase/firebase";
 const CodeReviewCard = dynamic(() => import("./CodeReviewCard"));
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+} from "firebase/firestore/lite";
 
-const ViewFeed = () => {
+interface Post {
+  id: string;
+  title: string;
+  upVotes: number;
+  createdBy: string;
+  createdAt: string;
+}
+
+const ViewFeed: any = async () => {
+  const postsRef = collection(db, "discussions");
+  query(postsRef, orderBy("createdAt"), limit(20));
+  const [posts]: any = await getDocs(postsRef); // ! bug is right here
   return (
     <Container variant="regular">
       <div className="flex flex-col items-center justify-between m-2">
@@ -12,7 +31,16 @@ const ViewFeed = () => {
             Create a post
           </button>
         </div>
-        <CodeReviewCard />
+        {posts &&
+          posts.map((post: Post) => (
+            <CodeReviewCard
+              key={post.id}
+              title={post.title}
+              createdAt={post.createdAt}
+              createdBy={post.createdBy}
+              upVotes={post.upVotes}
+            />
+          ))}
       </div>
     </Container>
   );
