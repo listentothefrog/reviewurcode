e29b8acf7c4e7ccea32d5ad418659f19fe03b31d
listentothefrog <shashanke7y@gmail.com>
2021-10-27 17:09:29 -0400

    removed createdAt prop

--- components/Dashboard/CodeReviewCard.tsx
+++ components/Dashboard/CodeReviewCard.tsx
@@ -1,28 +1,25 @@
-import { auth } from "../../lib/firebase/firebase";
-import Image from "next/image";
-
 interface Post {
   title: string;
   upVotes: number;
   createdBy: string;
-  createdAt: string;
+  codeReviewImage: string;
+  photoUrl: string;
 }
 
 const CodeReviewCard: React.FC<Post> = (props) => {
-  const { title, upVotes, createdAt, createdBy } = props;
+  const { title, upVotes, createdBy, codeReviewImage, photoUrl } = props;
   return (
-    <div className="container my-12 mx-auto px-4 md:px-12">
+    <div className="container mt-2 mx-auto px-4 md:px-12">
       <div className="flex flex-wrap -mx-1 lg:-mx-4">
         <div className="my-1 px-1 w-full lg:my-4 lg:px-4">
           <article className="overflow-hidden rounded-lg shadow-lg">
             <a href="#">
-              <Image
+              <img
+                src={codeReviewImage! as string}
                 width="600px"
                 height="500px"
-                objectFit="cover"
                 alt="code block"
-                className="block h-auto w-full"
-                src={auth.currentUser?.photoURL! as string}
+                className="block h-auto w-full object-cover"
               />
             </a>
 
@@ -32,29 +29,22 @@ const CodeReviewCard: React.FC<Post> = (props) => {
                   {title}
                 </a>
               </h1>
-              <p className="text-grey-darker text-sm">{createdAt}</p>
             </header>
 
-            <footer className="flex items-center justify-between leading-none p-2 md:p-4">
-              <a
-                className="flex items-center no-underline hover:underline text-black"
-                href="#"
-              >
-                <Image
+            <footer className="flex items-center justify-between leading-none md:p-4">
+              <a className="flex items-center no-underline hover:underline text-black">
+                <img
                   width="28"
                   height="28"
                   alt="Profile picture"
                   className="block rounded-full w-7"
-                  src={auth.currentUser?.photoURL! as string}
+                  src={photoUrl}
                 />
                 <p className="ml-2 text-sm">{createdBy}</p>
               </a>
-              <a
-                className="no-underline text-grey-darker hover:text-red-dark"
-                href="#"
-              >
-                <span className="hidden">{upVotes}</span>
-              </a>
+              <p className="ml-2 text-sm">
+                <span className="text-green-600">{upVotes}</span> upvotes
+              </p>
             </footer>
           </article>
         </div>
