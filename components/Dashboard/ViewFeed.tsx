f08e3bd5036d7194adfca6a9f2c9ac20ca11264e
listentothefrog <shashanke7y@gmail.com>
2021-10-27 18:41:51 -0400

    link to create post route

--- components/Dashboard/ViewFeed.tsx
+++ components/Dashboard/ViewFeed.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { db } from "../../lib/firebase/firebase";
 import Container from "../Container";
 import CodeReviewCard from "./CodeReviewCard";
+import Link from "next/link";
 
 const ViewFeed = () => {
   const [posts, setPosts] = useState([]);
@@ -23,9 +24,11 @@ const ViewFeed = () => {
       <div className="flex flex-col items-center justify-between m-2">
         <div className="flex items-center justify-between w-full h-full">
           <h1 className="text-xl font-medium">Top Posts</h1>
-          <button className="px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-500 hover:bg-red-600 md:py-4 md:text-lg md:px-10 transition duration-500 ease-in-out">
-            Create a post
-          </button>
+          <Link href="/create">
+            <button className="px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-500 hover:bg-red-600 md:py-4 md:text-lg md:px-10 transition duration-500 ease-in-out">
+              Create a post
+            </button>
+          </Link>
         </div>
         {posts.map((data: any) => (
           <CodeReviewCard
