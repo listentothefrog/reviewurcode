c227fbc769bfc035eec9ed706024c122e9a0d852
listentothefrog <shashanke7y@gmail.com>
2021-10-27 21:30:52 -0400

    import CreateCodeReviewPost component

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,5 +1,14 @@
+import dynamic from "next/dynamic";
+const CreateCodeReviewPost = dynamic(
+  () => import("../components/dashboard/CreateCodeReviewPost")
+);
+
 const create = () => {
-  return <div>Wsp</div>;
+  return (
+    <div>
+      <CreateCodeReviewPost />
+    </div>
+  );
 };
 
 export default create;
