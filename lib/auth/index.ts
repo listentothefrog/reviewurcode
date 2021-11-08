d653128560f020fc94b986606c93307e7420bfa3
listentothefrog <shashanke7y@gmail.com>
2021-11-08 16:23:20 -0500

    remove console.log

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -4,20 +4,14 @@ import { auth, db } from "../firebase/firebase";
 
 export const githubOAuth = async () => {
   await signInWithPopup(auth, new GithubAuthProvider())
-    .then(async (result) => {
-      const user = result;
-      console.log(user);
+    .then(async () => {
       await setDoc(doc(db, "users", auth.currentUser?.uid! as any), {
         username: auth.currentUser?.displayName,
         photoUrl: auth.currentUser?.photoURL,
         createdAt: auth.currentUser?.metadata.creationTime,
       });
     })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      const email = error.email;
-      const credential = GithubAuthProvider.credentialFromError(error);
-      console.log(errorCode, errorMessage, email, credential);
+    .catch((error: Error) => {
+      console.error(error.message);
     });
 };
