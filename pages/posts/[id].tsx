c5d7fa896bbd9e9f45e0ed30d82b3973dea0dc15
Shashank <shashanke7y@gmail.com>
2021-11-12 18:58:33 -0600

    dynamic post routes

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,5 +1,30 @@
-const post = () => {
-  return <div>Hey</div>;
+import { getDoc, doc } from "@firebase/firestore";
+import { GetServerSideProps } from "next";
+import { db } from "../../lib/firebase/firebase";
+
+
+export const getServerSideProps: GetServerSideProps = async (ctx: any) => {
+  const docRef = doc(db, "posts", ctx.query.id);
+  const docSnap = await getDoc(docRef);
+  const data = docSnap.data()
+  console.log(data)
+  if (!data) return { notFound: true };
+  return { props: { data } };
 };
 
-export default post;
+const Post = ({data}: any) => {
+  if (!data) {
+    return "Loading...";
+  }
+
+  return (
+    <div>
+      <p>Title: {data.title}</p>
+      <p>Description: {data.body}</p>
+      <img src={data.codeReviewImage} alt="code snippet" />
+      <p>Author: {data.createdBy}</p>
+    </div>
+  );
+}
+
+export default Post;
